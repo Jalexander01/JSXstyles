ccbedd7d3296af4be176d6c49a0e2be4c87865e7
James Alexander <60295314+Jalexander01@users.noreply.github.com>
2021-06-08 14:36:54 -0500

    adding new pictures

--- src/index.js
+++ src/index.js
@@ -8,22 +8,25 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+const img = "https://picsum.photos/200";
+
 const name = "Jimbo";
 ReactDom.render(
   <div>
-    <h1>Here</h1>
-    <p>
-      Geckos are a group of usually small, usually nocturnal lizards. They are
-      found on every continent except Australia.
-    </p>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      Here
+    </h1>
+    <div>
+      <img
+        className="lamps "
+        src="https://www.w3schools.com/images/lamp.jpg"
+        alt="Lamp"
+      />
 
-    <p>
-      Some species live in houses where they hunt insects attracted by
-      artificial light.
-    </p>
-    <p>
-      {name} x {getRandomInt(5)}
-    </p>
+      <img className="images " src={img} alt="Lamp" />
+
+      <img className="images" src={img} alt="ramdom" />
+    </div>
 
     <p>Copyright {n}</p>
   </div>,
